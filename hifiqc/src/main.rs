3403d224ee804fe8fe6d990ef9185c1d75c11a11
Casey Dunn <casey.dunn@yale.edu>
2023-03-06 22:45:43 -0500

    started bam reader

---
+++ hifiqc/src/main.rs
@@ -0,0 +1,22 @@
+//use bam::BamReader;
+use std::env;
+
+fn main() {
+    // Get the filename from command line arguments
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+
+    // Open the BAM file
+    //let mut bam_reader = bam::BamReader::from_path(filename).unwrap();
+    let bam_reader = bam::BamReader::from_path(filename, 4).unwrap();
+
+    // Loop through each record in the BAM file
+    for record_result in bam_reader {
+        // Unwrap the record result
+        let record = record_result.unwrap();
+
+        // Print the record information
+        println!("{}:{}-{} {}", record.tid(), record.pos(), record.cigar().end_pos(), record.seq().len());
+    }
+}
+
