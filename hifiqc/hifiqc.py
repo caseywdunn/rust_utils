b293933c2bbf16361e591ff30364e16962b03f29
Casey Dunn <caseywdunn@gmail.com>
2023-03-08 11:32:59 -0500

    more plots

--- hifiqc/hifiqc.py
+++ hifiqc/hifiqc.py
@@ -35,11 +35,54 @@ def main() -> int:
     # read the input file
     df = pd.read_csv(in_file, sep=' ', header=0, index_col=0)
     
-    ax = df.plot.hexbin(x='length', y='readscore', gridsize=20)
-    
-    # Save the plot
+    # Generate a histogram of read lengths
+    hist_len = df['length'].plot.hist(bins=100)
+    fig_hist_len = hist_len.get_figure()
+    fig_hist_len.axes[0].set_xlabel("length")
+    fig_hist_len.savefig(out_file_name + ".hist_length.png")
+    fig_hist_len.clf()
+
+    # Generate a histogram of read scores
+    hist_readscore = df['readscore'].plot.hist(bins=100)
+    fig_hist_readscore = hist_readscore.get_figure()
+    fig_hist_readscore.axes[0].set_xlabel("readscore")
+    fig_hist_readscore.savefig(out_file_name + ".hist_readscore.png")
+    fig_hist_readscore.clf()
+
+    # generate a histogram of np
+    hist_np = df['np'].plot.hist(bins=100)
+    fig_hist_np = hist_np.get_figure()
+    fig_hist_np.axes[0].set_xlabel("np")
+    fig_hist_np.savefig(out_file_name + ".hist_np.png")
+    fig_hist_np.clf()
+
+    # Generate a histogram of quality_mean
+    hist_qm = df['quality_mean'].plot.hist(bins=100)
+    fig_hist_qm = hist_qm.get_figure()
+    fig_hist_qm.axes[0].set_xlabel("quality_mean")
+    fig_hist_qm.savefig(out_file_name + ".hist_quality_mean.png")
+    fig_hist_qm.clf()
+
+    # hexbin plots
+    ax = df.plot.hexbin(x='length', y='np', gridsize=20, bins='log')
+    fig = ax.get_figure()
+    fig.savefig(out_file_name + ".np_v_length.png")
+    fig.clf()
+
+    ax = df.plot.hexbin(x='length', y='readscore', gridsize=20, bins='log')
     fig = ax.get_figure()
     fig.savefig(out_file_name + ".readscore_v_length.png")
+    fig.clf()
+
+    ax = df.plot.hexbin(x='np', y='readscore', gridsize=20, bins='log')
+    fig = ax.get_figure()
+    fig.savefig(out_file_name + ".readscore_v_np.png")
+    fig.clf()
+
+    ax = df.plot.hexbin(x='quality_mean', y='readscore', gridsize=20, bins='log')
+    fig = ax.get_figure()
+    fig.savefig(out_file_name + ".readscore_v_qualitymean.png")
+    fig.clf()
     
     # Open the output log file
     with open(out_file_name + ".log", 'w') as log_file:
